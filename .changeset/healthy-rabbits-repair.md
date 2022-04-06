c6f5376e713aceb012507c23e34b86dcc4274745
Eric <43848365+eegli@users.noreply.github.com>
2022-04-06 19:49:32 +0200

    Release packages

---
+++ .changeset/healthy-rabbits-repair.md
@@ -0,0 +1,8 @@
+---
+'@spotifly/auth-token': minor
+'@spotifly/cli': minor
+'@spotifly/library': minor
+'@spotifly/utils': minor
+---
+
+Initial release
--- docs/src/theme/mui-theme-wrapper.tsx
+++ docs/src/theme/mui-theme-wrapper.tsx
@@ -1,39 +1,36 @@
 import { useColorMode } from '@docusaurus/theme-common';
-import {
-  createTheme,
-  PaletteMode,
-  ThemeOptions,
-  ThemeProvider,
-} from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material';
 import React, { ReactNode, useMemo } from 'react';
 
-// Keep in sync with custom.css color definitions
-const getDesignTokens = (mode: PaletteMode): ThemeOptions => ({
+const darkMode = createTheme({
   palette: {
-    mode,
-    ...(mode === 'light'
-      ? {
-          primary: {
-            main: '#4a44a2',
-            light: '#514bb2',
-            dark: '#433d92',
-          },
-        }
-      : {
-          primary: {
-            main: '#e6b2e2',
-            light: '#f0d1ee',
-            dark: '#dc93d6',
-          },
-        }),
+    mode: 'dark',
+    primary: {
+      main: '#e6b2e2',
+      light: '#f0d1ee',
+      dark: '#dc93d6',
+    },
+  },
+});
+
+const lightMode = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#4a44a2',
+      light: '#514bb2',
+      dark: '#433d92',
+    },
   },
 });
 
 export const MuiThemeWrapper = ({ children }: { children: ReactNode }) => {
   const { colorMode } = useColorMode();
+
   const theme = useMemo(
-    () => createTheme(getDesignTokens(colorMode)),
+    () => (colorMode === 'light' ? lightMode : darkMode),
     [colorMode]
   );
+
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
