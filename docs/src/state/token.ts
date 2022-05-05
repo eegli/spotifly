ae57ca2ad911e2e33c609bb7a1a61774a2f53be9
Eric <43848365+eegli@users.noreply.github.com>
2022-05-05 19:54:19 +0200

    fix linter stuff

--- docs/src/state/token.ts
+++ docs/src/state/token.ts
@@ -20,6 +20,7 @@ type TokenState = {
   scopes: string[];
   error: string;
 };
+
 export function tokenReducer(state: TokenState, action: TokenAction) {
   const { type, payload } = action;
   switch (type) {
@@ -29,9 +30,10 @@ export function tokenReducer(state: TokenState, action: TokenAction) {
       return { ...state, clientSecret: payload };
     case 'setRefreshToken':
       return { ...state, refreshToken: payload };
-    case 'setScopes':
+    case 'setScopes': {
       const scopes = typeof payload === 'string' ? [payload] : payload;
       return { ...state, scopes };
+    }
     case 'setError':
       return { ...state, error: payload };
     default:
