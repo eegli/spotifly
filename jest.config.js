021530a8afa55c58d1b48fe27ea8bf10ea89bf04
Eric <lx180777+git@gmail.com>
2022-07-01 20:40:20 +0200

    wip

--- jest.config.js
+++ jest.config.js
@@ -52,11 +52,5 @@ module.exports = {
       roots: ['<rootDir>/src', '<rootDir>/test'],
       collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
     },
-    // TODO run docs test separately
-    /* {
-      ...common,
-      displayName: 'docs',
-      rootDir: 'docs',
-    }, */
   ],
 };
