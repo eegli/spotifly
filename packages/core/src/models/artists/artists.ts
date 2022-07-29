b28cf3ae855de560a7e91b7d1cfddb5405558d84
eegli <lx180777+git@gmail.com>
2022-07-29 21:54:28 +0200

    wrap up artists

--- packages/core/src/models/artists/artists.ts
+++ packages/core/src/models/artists/artists.ts
@@ -25,3 +25,52 @@ export const getMultipleArtists: WithProvider<
     })
   );
 export const getMultipleArtistsLimit = 50;
+
+type GroupType = 'album' | 'single' | 'appears_on' | 'compilation';
+type Group =
+  | `${GroupType}`
+  | `${GroupType},${GroupType}`
+  | `${GroupType},${GroupType},${GroupType}`
+  | `${GroupType},${GroupType},${GroupType},${GroupType}`;
+
+export const getArtistsAlbums: WithProvider<
+  {
+    artistId: string;
+    include_groups?: Group;
+    offset?: number;
+    limit?: number;
+    market?: string;
+  },
+  SpotifyApi.ArtistsAlbumsResponse
+> = provider => async params =>
+  transformResponse(
+    await provider.request({
+      method: Methods.GET,
+      url: `artists/${params.artistId}/albums`,
+      params,
+    })
+  );
+export const getArtistsAlbumsLimit = 50;
+
+export const getArtistsTopTracks: WithProvider<
+  { artistId: string; market?: string },
+  SpotifyApi.ArtistsTopTracksResponse
+> = provider => async params =>
+  transformResponse(
+    await provider.request({
+      method: Methods.GET,
+      url: `artists/${params.artistId}/top-tracks`,
+      params,
+    })
+  );
+
+export const getArtistsRelatedArtists: WithProvider<
+  { artistId: string },
+  SpotifyApi.ArtistsRelatedArtistsResponse
+> = provider => async params =>
+  transformResponse(
+    await provider.request({
+      method: Methods.GET,
+      url: `artists/${params.artistId}/related-artists`,
+    })
+  );
