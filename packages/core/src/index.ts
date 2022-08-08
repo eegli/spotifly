fd8025f561004e6163b04754cdf5d736494e3a4f
eegli <lx180777+git@gmail.com>
2022-08-08 18:58:32 +0200

    cleanup

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,3 @@
-export { init, mockInit } from './ignition';
+export { default as init } from './ignition';
 export type { AuthInitOptions } from './provider';
 export type { DataPromise, DataResponse } from './types';
