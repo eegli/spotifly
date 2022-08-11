9f665dbf888a60e89a5deeff5e0d93943ca58831
Eric Egli <43848365+eegli@users.noreply.github.com>
2022-08-11 08:07:15 +0200

    ci: configure codecov (#32)

--- packages/integration-tests/core-readme.test.ts
+++ packages/integration-tests/core-readme.test.ts
@@ -1,9 +1,63 @@
-import * as Spotify from '@spotifly/core';
+import * as Spotifly from '@spotifly/core';
+import { mockDeep } from 'jest-mock-extended';
+
+const mockSpotify = mockDeep<Spotifly.SpotifyClient>();
+
+type MockResponse =
+  Spotifly.DataResponse<SpotifyApi.MultipleAudioFeaturesResponse>;
+
+const mockResponse: MockResponse = {
+  data: {
+    audio_features: [
+      {
+        acousticness: 0.00242,
+        analysis_url: 'https://api.spotify.com/v1/audio-analysis/2takc7B',
+        danceability: 0.585,
+        duration_ms: 237040,
+        energy: 0.842,
+        id: '2takc7B',
+        instrumentalness: 0.00686,
+        key: 9,
+        liveness: 0.0866,
+        loudness: -5.883,
+        mode: 0,
+        speechiness: 0.0556,
+        tempo: 118.211,
+        time_signature: 4,
+        track_href: 'https://api.spotify.com/v1/tracks/2takc7B',
+        type: 'audio_features',
+        uri: 'spotify:track:2takc7B',
+        valence: 0.428,
+      },
+    ],
+  },
+  statusCode: 200,
+  headers: {},
+};
+
+jest.spyOn(Spotifly, 'initialize').mockReturnValue(mockSpotify);
+
+// Mocking an ordinary method
+mockSpotify.Tracks.AudioFeatures.getSeveral.mockImplementation(() => {
+  return Promise.resolve(mockResponse);
+});
+
+// Mocking a convenience method
+mockSpotify.Tracks.AudioFeatures.getAll.mockImplementation(() => {
+  return cb => {
+    if (cb) cb(mockResponse);
+    return Promise.resolve([mockResponse]);
+  };
+});
+
+describe('Test', () => {
+  // ...
+});
 
 describe('Core readme', () => {
   test('general example', async () => {
     {
-      const spotifyClient = Spotify.initialize({
+      const spotifyClient = Spotifly.initialize({
         clientId: process.env.SPOTIFY_CLIENT_ID || '',
         clientSecret: process.env.SPOTIFY_CLIENT_SECRET || '',
         refreshToken: process.env.SPOTIFY_REFRESH_TOKEN || '',
@@ -31,7 +85,7 @@ describe('Core readme', () => {
         });
     }
     {
-      const spotifyClient = Spotify.initialize({
+      const spotifyClient = Spotifly.initialize({
         accessToken: 'abc123',
       });
     }
