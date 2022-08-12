4e0933cf08ebee73263a29170f40fd05cb952ae4
eegli <lx180777+git@gmail.com>
2022-08-12 16:00:25 +0200

    wip

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -1,10 +1,9 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-  ...require('./jest.config').__common,
-  /*   displayName: {
+  ...require('../jest.config').__common,
+  displayName: {
     name: 'integration',
     color: 'magenta',
-  }, */
-  rootDir: 'packages/integration-tests',
+  },
   setupFiles: ['dotenv/config'],
 };
