f4e62e75fda7591148dee350a629b1bf6a5bd8de
eegli <lx180777+git@gmail.com>
2022-09-02 16:44:59 +0200

    fix types

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,62 +1,66 @@
 import { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-export type DataResponse<T = unknown> = {
-  data: T;
+export type DataResponse<Data = unknown> = {
+  data: Data;
   headers: Record<string, string>;
   statusCode: number;
 };
 
-export type DataPromise<T = unknown> = Promise<DataResponse<T>>;
+export type DataPromise<Data = unknown> = Promise<DataResponse<Data>>;
 
 export type AsyncProvider = {
   request(req: AxiosRequestConfig): Promise<AxiosResponse>;
 };
 
 export type AsyncFn<
-  R,
-  P1 = undefined,
-  P2 = undefined,
-  P3 = undefined,
-  RT = DataPromise<R>
-> = P1 extends undefined
-  ? () => RT
-  : P2 extends undefined
-  ? (required: P1) => RT
-  : P3 extends undefined
-  ? P2 extends AnyObject
-    ? (required: P1, optional?: Partial<P2>) => RT
-    : (required1: P1, required2: P2) => RT
-  : P3 extends AnyObject
-  ? (required1: P1, required2: P2, optional?: Partial<P3>) => RT
+  ReturnValue,
+  Param1 = undefined,
+  Param2 = undefined,
+  Param3 = undefined,
+  ReturnType = DataPromise<ReturnValue>
+> = Param1 extends undefined
+  ? () => ReturnType
+  : Param2 extends undefined
+  ? (required: Param1) => ReturnType
+  : Param3 extends undefined
+  ? Param2 extends AnyObject
+    ? (required: Param1, optional?: Partial<Param2>) => ReturnType
+    : (required1: Param1, required2: Param2) => ReturnType
+  : Param3 extends AnyObject
+  ? (
+      required1: Param1,
+      required2: Param2,
+      optional?: Partial<Param3>
+    ) => ReturnType
   : never;
 
 export type AsyncFnWithProvider<
-  R,
-  A = undefined,
-  B = undefined,
-  C = undefined
-> = (provider: AsyncProvider) => AsyncFn<R, A, B, C>;
+  ReturnValue,
+  Param1 = undefined,
+  Param2 = undefined,
+  Param3 = undefined
+> = (provider: AsyncProvider) => AsyncFn<ReturnValue, Param1, Param2, Param3>;
 
 export type AnyObject = Record<string, unknown>;
 
 export type AnyFunc = (...args: any[]) => unknown;
 
 // https://github.com/microsoft/TypeScript/issues/39556
-export type BetterOmit<T, E> = {
-  [P in keyof T as Exclude<P, E>]: T[P];
+export type BetterOmit<Type, Key> = {
+  [P in keyof Type as Exclude<P, Key>]: Type[P];
 };
 
 export type OmitFromAsyncFnParams<
-  F extends AnyFunc,
-  O extends AnyObject
-> = F extends (arg1: infer A, arg2: infer B) => unknown
-  ? [A, BetterOmit<B, keyof O>?]
+  Func extends AnyFunc,
+  Object extends AnyObject
+> = Func extends (arg1: infer FirstArg, arg2: infer SecondArg) => unknown
+  ? [FirstArg, BetterOmit<SecondArg, keyof Object>?]
   : never;
 
-export type ReadOnlyParams<T extends AnyFunc> = T extends (
-  ...args: infer P
+export type ReadOnlyParams<Func extends AnyFunc> = Func extends (
+  ...args: infer Params
 ) => unknown
-  ? Readonly<P>
+  ? Readonly<Params>
   : never;
 
 // Branded types - T = T & {} - improve IntelliSense experience
@@ -90,6 +94,6 @@ export type After = { after: string };
 export type Limit = { limit: number };
 export type Offset = { offset: number };
 
-export type SinglePropertyResponse<K extends string> = {
-  [P in K]: string[];
+export type SinglePropertyResponse<Key extends string> = {
+  [Property in Key]: string[];
 };
