9b941fe889d1e1ef40b749278fade3df93c72bd3
eegli <lx180777+git@gmail.com>
2022-09-03 15:17:11 +0200

    future method and docs

--- packages/core/src/models/future.ts
+++ packages/core/src/models/future.ts
@@ -2,6 +2,7 @@ import { AxiosRequestConfig } from 'axios';
 import { transformResponse } from '../request';
 import { AsyncProvider } from '../types';
 
+/** Generic convenience method that allows freestyle requests which include an access token. Can be used to call new Spotify endpoints that are not yet supported by this library. */
 export default function futureRequest(provider: AsyncProvider) {
   return {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
