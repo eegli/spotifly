c186b4f35a1025266a9a6d05af30ac31d426870a
eegli <lx180777+git@gmail.com>
2022-09-05 13:46:25 +0200

    fix gh action and improve error handling

--- packages/library/src/handler.ts
+++ packages/library/src/handler.ts
@@ -1,111 +1,126 @@
 import * as Spotify from '@spotifly/core';
-import { writeJSON } from '@spotifly/utils';
+import { colors, writeJSON } from '@spotifly/utils';
 import { defaultConfig } from './config';
 import type { Library, LibraryHandler, TrackLight } from './types';
 import { createProgressBar } from './utils';
 
 export const libraryHandler: LibraryHandler = async options => {
-  const config = { ...defaultConfig, ...options };
+  try {
+    const config = { ...defaultConfig, ...options };
 
-  const spotifyClient = Spotify.initialize({ accessToken: config.token });
+    const spotifyClient = Spotify.initialize({ accessToken: config.token });
 
-  let library: Library = [];
+    let library: Library = [];
 
-  let progress = createProgressBar('user library');
+    let progress = createProgressBar('user library');
 
-  progress.start(0, 0);
+    progress.start(0, 0);
 
-  await spotifyClient.Tracks.getAllUsersSavedTracks()(({ data }) => {
-    progress.setTotal(data.total);
-    progress.increment(data.items.length);
+    await spotifyClient.Tracks.getAllUsersSavedTracks()(({ data }) => {
+      progress.setTotal(data.total);
+      progress.increment(data.items.length);
 
-    data.items.forEach(el => {
-      library.push(el);
+      data.items.forEach(el => {
+        library.push(el);
+      });
     });
-  });
 
-  progress.stop();
+    progress.stop();
 
-  // Reduce library if necessary
-  if (config.type === 'light') {
-    library = library.reduce((acc, curr) => {
-      acc.push({
-        added_at: curr.added_at,
-        track: {
-          id: curr.track.id,
-          name: curr.track.name,
-          album: {
-            id: curr.track.album.id,
-            name: curr.track.album.name,
+    // Reduce library if necessary
+    if (config.type === 'light') {
+      library = library.reduce((acc, curr) => {
+        acc.push({
+          added_at: curr.added_at,
+          track: {
+            id: curr.track.id,
+            name: curr.track.name,
+            album: {
+              id: curr.track.album.id,
+              name: curr.track.album.name,
+            },
+            artists: curr.track.artists.map(a => ({
+              id: a.id,
+              name: a.name,
+            })),
           },
-          artists: curr.track.artists.map(a => ({
-            id: a.id,
-            name: a.name,
-          })),
-        },
+        });
+        return acc;
+      }, <Library<TrackLight>>[]);
+    }
+
+    // Add genres if specified
+    if (config.genres) {
+      const artists = library.map(t => t.track.artists.map(a => a.id)).flat();
+      const artistIds = [...new Set<string>(artists)];
+      const genres: Record<string, string[]> = {};
+
+      progress = createProgressBar('artists');
+      progress.start(artistIds.length, 0);
+
+      await spotifyClient.Artists.getAllArtists(artistIds)(({ data }) => {
+        data.artists.forEach(artist => {
+          genres[artist.id] = artist.genres;
+        });
+        progress.increment(data.artists.length);
       });
-      return acc;
-    }, <Library<TrackLight>>[]);
-  }
 
-  // Add genres if specified
-  if (config.genres) {
-    const artists = library.map(t => t.track.artists.map(a => a.id)).flat();
-    const artistIds = [...new Set<string>(artists)];
-    const genres: Record<string, string[]> = {};
-
-    progress = createProgressBar('artists');
-    progress.start(artistIds.length, 0);
-
-    await spotifyClient.Artists.getAllArtists(artistIds)(({ data }) => {
-      data.artists.forEach(artist => {
-        genres[artist.id] = artist.genres;
+      progress.stop();
+      library.forEach(({ track }) => {
+        track.genres = track.artists.map(({ id }) => genres[id]);
       });
-      progress.increment(data.artists.length);
-    });
+    }
 
-    progress.stop();
-    library.forEach(({ track }) => {
-      track.genres = track.artists.map(({ id }) => genres[id]);
-    });
-  }
+    // Add audio features if specified
+    if (config.features) {
+      const trackIds = library.map(t => t.track.id);
+      const features: Record<string, SpotifyApi.AudioFeaturesObject> = {};
 
-  // Add audio features if specified
-  if (config.features) {
-    const trackIds = library.map(t => t.track.id);
-    const features: Record<string, SpotifyApi.AudioFeaturesObject> = {};
+      progress = createProgressBar('audio features');
 
-    progress = createProgressBar('audio features');
+      progress.start(trackIds.length, 0);
 
-    progress.start(trackIds.length, 0);
-
-    await spotifyClient.Tracks.getAllAudioFeatures(trackIds)(({ data }) => {
-      data.audio_features.forEach(f => {
-        features[f.id] = f;
+      await spotifyClient.Tracks.getAllAudioFeatures(trackIds)(({ data }) => {
+        data.audio_features.forEach(f => {
+          features[f.id] = f;
+        });
+        progress.increment(data.audio_features.length);
       });
-      progress.increment(data.audio_features.length);
-    });
 
-    progress.stop();
-    library.forEach(({ track }) => {
-      track.features = features[track.id];
+      progress.stop();
+      library.forEach(({ track }) => {
+        track.features = features[track.id];
+      });
+    }
+
+    const libExport = {
+      meta: {
+        date_generated: new Date().toISOString(),
+        output_type: config.type,
+      },
+      library,
+    };
+
+    const outDir = await writeJSON({
+      fileName: 'spotify-library',
+      path: config.outDir,
+      data: libExport,
+      compact: config.compact,
     });
+    console.info("Success! Library written to '%s'", outDir);
+    return libExport;
+  } catch (error) {
+    if (Spotify.isError(error)) {
+      console.error(
+        colors.red(
+          `\nError talking to Spotify:\n${JSON.stringify(
+            error.response?.data.error,
+            null,
+            2
+          )}`
+        )
+      );
+    }
+    throw error;
   }
-
-  const libExport = {
-    meta: {
-      date_generated: new Date().toISOString(),
-      output_type: config.type,
-    },
-    library,
-  };
-
-  const outDir = await writeJSON({
-    fileName: 'spotify-library',
-    path: config.outDir,
-    data: libExport,
-    compact: config.compact,
-  });
-  console.info("Success! Library written to '%s'", outDir);
-  return libExport;
 };
