2ad40c6e3062d14bbff9b6886744126325b71c78
Eric Egli <43848365+eegli@users.noreply.github.com>
2022-12-06 08:24:40 +0100

    experiments

--- packages/playlit/src/index.ts
+++ packages/playlit/src/index.ts
@@ -1,3 +1,7 @@
+import _Data from '../test/payloads/data.json';
+
+const Data = _Data as SpotifyApi.AudioFeaturesObject[];
+
 type AudioFeatures = SpotifyApi.AudioFeaturesObject;
 type Weights = Record<keyof AudioFeatures, number>;
 type RequireAtLeastOne<T> = {
@@ -14,3 +18,23 @@ const options: Options = {
     acousticness: 1,
   },
 };
+
+const add = (a: number, b: number) => a + b;
+
+function standardize<T extends number[]>(data: T): T {
+  const N = data.length;
+  const mean = data.reduce(add) / N;
+  const variance = data.map(x => (x - mean) ** 2).reduce(add) / N;
+  const stdized = data.map(x => (x - mean) / Math.sqrt(variance));
+  return stdized as T;
+}
+
+function getLargestAbsValue<T extends number[]>(data: T): number {
+  return Math.max(...data.map(Math.abs));
+}
+export default function () {
+  const acousticness = Data.map(el => el.acousticness);
+  console.log({ red: acousticness });
+  console.log(standardize(acousticness));
+  console.log(getLargestAbsValue(acousticness));
+}
--- packages/playlit/test/index.test.ts
+++ packages/playlit/test/index.test.ts
@@ -1,1 +1,7 @@
-export {};
+import playlit from '../src';
+
+describe('Playlit', () => {
+  test('It works', () => {
+    playlit();
+  });
+});
